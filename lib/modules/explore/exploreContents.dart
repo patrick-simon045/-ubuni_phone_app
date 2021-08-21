984fb4c1fabe75d1d9a5f0401ee53bef9495d652
Patrick Simon <patricksimon045@gmail.com>
2021-08-21 12:25:51 -0700

    created phone brand category list on phone list screen

--- lib/modules/explore/exploreContents.dart
+++ lib/modules/explore/exploreContents.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:ubuni_phone_app/components/text.dart';
 import 'package:ubuni_phone_app/globals/padding.dart';
 import 'package:ubuni_phone_app/models/phone.dart';
 
@@ -15,8 +15,20 @@ class ExploreContents extends StatefulWidget {
 }
 
 class _ExploreContentsState extends State<ExploreContents> {
+  Set<String> _phoneBrandCategories = Set();
+  int _selectedCategoryIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    widget.phones.forEach((phone) {
+      _phoneBrandCategories.add(phone.phoneBrandName);
+    });
+    _phoneBrandCategories.add("All");
+
+    // phone brand list
+    final _phoneBrandCategoriesAsList = _phoneBrandCategories.toList();
+    _phoneBrandCategoriesAsList.sort();
+
     final dimension = smallPadding;
 
     return Padding(
@@ -24,6 +36,49 @@ class _ExploreContentsState extends State<ExploreContents> {
       child: ListView(
         physics: BouncingScrollPhysics(),
         children: [
+          Container(
+            height: 50.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              physics: BouncingScrollPhysics(),
+              children: _phoneBrandCategoriesAsList.map(
+                (category) {
+                  int categoryIndex =
+                      _phoneBrandCategoriesAsList.indexOf(category);
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _selectedCategoryIndex = categoryIndex;
+                      });
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.only(
+                        left: _selectedCategoryIndex == categoryIndex
+                            ? smallPadding * 2
+                            : 0.0,
+                        right: _selectedCategoryIndex == categoryIndex
+                            ? smallPadding * 2
+                            : smallPadding,
+                      ),
+                      child: TextWidget(
+                        text: "$category",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: _selectedCategoryIndex == categoryIndex
+                              ? 50.0
+                              : 40.0,
+                          color: _selectedCategoryIndex == categoryIndex
+                              ? Colors.grey
+                              : Colors.grey.withOpacity(0.5),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
+          ),
           ProductList(
             phones: widget.phones,
             dimension: dimension,
@@ -33,4 +88,3 @@ class _ExploreContentsState extends State<ExploreContents> {
     );
   }
 }
-
